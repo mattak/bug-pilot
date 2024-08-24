33e23f27bbfd2e9b0a939233f8f59026e29c83a9
mattak <mattak.me@gmail.com>
2024-08-24 17:43:07 +0900

    :seedling: download logs from current job

--- src/index.ts
+++ src/index.ts
@@ -4,49 +4,96 @@ import * as fs from 'fs';
 import {spawnSync} from "node:child_process";
 import {writeSummary} from "./summary_writer";
 import {getExecType, parseInput, validateInput} from "./input";
+import axios from "axios";
+import path from "node:path";
 
 async function run() {
   try {
-    const input = parseInput();
-    const logFile = input.logFile;
-    core.info(`Input: ${JSON.stringify(input.logFile)}!`);
-    core.info(`Event: ${github.context.eventName}`);
+    // 必要な入力を取得
+    const token = core.getInput('token', {required: true});
+    const runId = core.getInput('run_id', {required: true});
+    const owner = core.getInput('owner', {required: true});
+    const repo = core.getInput('repo', {required: true});
 
-    const [isValid, errorMessage] = validateInput(input);
-    if (!isValid) {
-      core.setFailed(errorMessage);
-      return;
+    // GitHub APIリクエストの設定
+    const apiUrl = `https://api.github.com/repos/${owner}/${repo}/actions/runs/${runId}`;
+    const headers = {
+      'User-Agent': 'Awesome-Curl-Client',
+      'Authorization': `token ${token}`,
+    };
+
+    // ログURLを取得
+    const response = await axios.get(apiUrl, {headers});
+    const logUrl = response.data.logs_url;
+
+    if (!logUrl) {
+      throw new Error('Failed to retrieve logs URL.');
     }
 
-    const platform = getExecType();
-    const execPath = `${__dirname}/bin/${platform}/loglint`;
-    const lintPath = `${__dirname}/.loglint.json`;
+    core.info(`Log URL: ${logUrl}`);
 
-    // spawnSyncでコマンドを実行し、inputとしてファイル内容を渡す
-    const inputContent = fs.readFileSync(logFile, {encoding: 'utf-8'});
-    const result = spawnSync(execPath, ['-f', lintPath], {
-      input: inputContent,
-      encoding: 'utf-8'
+    // ログファイルをダウンロード
+    const logResponse = await axios.get(logUrl, {
+      headers,
+      responseType: 'arraybuffer', // バイナリデータとして取得
     });
 
-    if (result.status === 0) {
-      console.log(`Stdout: ${result.stdout}`);
-      core.setOutput('result', result.stdout);
-      const lintResult = JSON.parse(result.stdout);
-      await writeSummary(lintResult, input);
-    } else {
-      console.log(`Stdout: ${result.stdout}`);
-      console.error(`Stderr: ${result.stderr}`);
-      core.setFailed(result.stderr);
-    }
+    const outputPath = path.join(process.cwd(), 'tmp.zip');
+    fs.writeFileSync(outputPath, logResponse.data);
 
+    core.info(`Logs have been downloaded to ${outputPath}`);
   } catch (error) {
     if (error instanceof Error) {
-      core.setFailed(error.message);
+      core.setFailed(`Action failed with error: ${error.message}`);
     } else {
       core.setFailed(`An unexpected error occurred: ${error}`);
     }
   }
 }
+
+
+// async function run() {
+//   try {
+//     const input = parseInput();
+//     const logFile = input.logFile;
+//     core.info(`Input: ${JSON.stringify(input.logFile)}!`);
+//     core.info(`Event: ${github.context.eventName}`);
+//
+//     const [isValid, errorMessage] = validateInput(input);
+//     if (!isValid) {
+//       core.setFailed(errorMessage);
+//       return;
+//     }
+//
+//     const platform = getExecType();
+//     const execPath = `${__dirname}/bin/${platform}/loglint`;
+//     const lintPath = `${__dirname}/.loglint.json`;
+//
+//     // spawnSyncでコマンドを実行し、inputとしてファイル内容を渡す
+//     const inputContent = fs.readFileSync(logFile, {encoding: 'utf-8'});
+//     const result = spawnSync(execPath, ['-f', lintPath], {
+//       input: inputContent,
+//       encoding: 'utf-8'
+//     });
+//
+//     if (result.status === 0) {
+//       console.log(`Stdout: ${result.stdout}`);
+//       core.setOutput('result', result.stdout);
+//       const lintResult = JSON.parse(result.stdout);
+//       await writeSummary(lintResult, input);
+//     } else {
+//       console.log(`Stdout: ${result.stdout}`);
+//       console.error(`Stderr: ${result.stderr}`);
+//       core.setFailed(result.stderr);
+//     }
+//
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       core.setFailed(error.message);
+//     } else {
+//       core.setFailed(`An unexpected error occurred: ${error}`);
+//     }
+//   }
+// }
 
 run();
